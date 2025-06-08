9a57c0fe4b631ff51f927ff415a0136351a206e6
parsa <hkh@arvand.com>
2025-06-08 11:09:04 +0330

    Version 1.2 Done

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,7 +6,7 @@
 using namespace WeaNet;
 
 int main() {
-
+    /** Receiver Config **/
     QThread* threadRecv = new QThread();
     Manager* receiver = new Manager();
     receiver->moveToThread(threadRecv);
@@ -15,6 +15,27 @@ int main() {
     receiver->onBind();
     receiver->onConnect();
 
+    QObject::connect(receiver, &Manager::readyRead, receiver, [=] () {
+            qDebug() << "Azimuth" << receiver->azimuth();
+            qDebug() << "Elv" << receiver->elevation();
+            qDebug() << "Range" << receiver->range();
+            //    qDebug() << "Time" <<
+            qDebug() << "Power" << receiver->power();
+            //    qDebug() << "ctime" << recvPackets.ctime;
+
+            QByteArray byte = receiver->rawData();
+            qDebug() << "RawData: " << byte.toHex();
+            QDataStream ds(&byte,QIODevice::ReadOnly);
+            ds.setByteOrder(QDataStream::LittleEndian);
+            double header;
+            ds >> header;
+            qDebug() << "header: " << header;
+            ds >> header;
+            qDebug() << "header: " << header;
+        }, Qt::DirectConnection);
+    threadRecv->start();
+
+    /** Sender Config **/
     QThread* threadSend = new QThread();
     Manager* sender = new Manager();
     sender->moveToThread(threadSend);
@@ -23,36 +44,11 @@ int main() {
     sender->setConnectionSetting(12345, 54321, "127.0.0.1"); // Replace with your own ip & ports
     sender->onBind();
     sender->onConnect(); // Assigning the peer port & address
-
-    threadRecv->start();
     threadSend->start();
-
-    //    std::vector<double> buff(1040, 0x00);
-//    double raw[7] = { 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7};
-//    size_t len = sizeof(raw);
-    //    QByteArray bytes(reinterpret_cast<const char*>(raw), len);
 
     sender->onSendLog("/home/Arvand/wearily/Log166Hub/docs/logData-slow.csv", 3); // Edit the path
     QThread::sleep(1);
 
-    QObject::connect(receiver, &Manager::readyRead, receiver, [=] () {
-        qDebug() << "Azimuth" << receiver->azimuth();
-        qDebug() << "Elv" << receiver->elevation();
-        qDebug() << "Range" << receiver->range();
-    //    qDebug() << "Time" <<
-        qDebug() << "Power" << receiver->power();
-    //    qDebug() << "ctime" << recvPackets.ctime;
-
-        QByteArray byte = receiver->rawData();
-        qDebug() << "RawData: " << byte.toHex();
-        QDataStream ds(&byte,QIODevice::ReadOnly);
-        ds.setByteOrder(QDataStream::LittleEndian);
-        double header;
-        ds >> header;
-        qDebug() << "header: " << header;
-        ds >> header;
-        qDebug() << "header: " << header;
-        }, Qt::DirectConnection);
 
     return 0;
 }

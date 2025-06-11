67cbd5e742a57849f3f1b2fe36d656cf5b47b234
parsa <hkh@arvand.com>
2025-06-11 10:01:52 +0330

    Install bash updated
    added: ignore Tests flag to install.sh shell.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INSTALL_PREFIX=${1:-/usr/local}
 
 mkdir -p build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -B build -S . -DBUILD_EXAMPLES=OFF
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -B build -S . -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF
 sudo cmake --build build --target uninstall
 sudo cmake --build build --config Release --target install
 

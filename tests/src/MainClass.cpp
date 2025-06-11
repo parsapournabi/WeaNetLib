d371cccd8e1a264ac08cf99a34e43f3ab5ffcbc6
parsa <hkh@arvand.com>
2025-06-11 15:37:54 +0330

    Typo fixed
    commented: useless things.

--- tests/src/MainClass.cpp
+++ tests/src/MainClass.cpp
@@ -23,15 +23,15 @@ MainClass::MainClass(QObject *parent)
 //    m_receiver->client->setReadTimeout(1);
 //    m_receiver->client->setMaxReadRetries(10);
 //    m_receiver->client->setAutoReconnect(true);
-    QObject::connect(m_receiver->client, SIGNAL(connected()), this, SLOT([=]() {qDebug() << "CLIENT CONNECTED" ; }));
-    QObject::connect(m_receiver->client, SIGNAL(disconnected()), this, SLOT([=]() {qDebug() << "CLIENT DISCONNECTED"<< m_receiver->client->autoReconnect();}));
+//    QObject::connect(m_receiver->client, SIGNAL(connected()), this, SLOT([=]() {qDebug() << "CLIENT CONNECTED" ; }));
+//    QObject::connect(m_receiver->client, SIGNAL(disconnected()), this, SLOT([=]() {qDebug() << "CLIENT DISCONNECTED"<< m_receiver->client->autoReconnect();}));
     QObject::connect(m_receiver, SIGNAL(readyReads(QSharedPointer<QList<QSharedPointer<LogDataType>>> logs,
                                                    qreal az,
                                                    qreal time)), this, SLOT([=] (QSharedPointer<QList<QSharedPointer<LogDataType>>> logs,
                                                                  qreal az,
                                                                  qreal time) {
         qDebug() << logs->size() << az << time;
-                     }));
+                     }), Qt::DirectConnection);
 
     m_receiver->onBind();
 //    m_receiver->onConnect();
